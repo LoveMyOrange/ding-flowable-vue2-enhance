55a56d9f681a0b0a656dd7f5bfb18ab01383e411
Gr <2471089198@qq.com>
2023-01-05 10:47:47 +0800

    fix 实现 主管选人, 角色找人的代码

--- src/main/java/com/dingding/mid/listener/CounterSignListener.java
+++ src/main/java/com/dingding/mid/listener/CounterSignListener.java
@@ -68,10 +68,19 @@ public class CounterSignListener implements ExecutionListener {
                 throw new WorkFlowException("暂不做这个功能,等发版!");
             }
             else if(AssigneeTypeEnums.LEADER.getTypeName().equals(assignedType)){
-                throw new WorkFlowException("当前只是简单的系统 没有RBAC功能,各位可以自己实现!");
+                /*
+                这里代码应该怎么写???   你想想
+                应该是 通过leader 的code  查出来leader里面的人  然后添加到
+                assigneeList.addAll()  既可
+                不过本项目没有添加leader的CRUD页面 所以 先这样,
+                怎么实现就是上述实现手段
+                下面角色也一样, 希望你们可以举一反三 一通百通
+                 */
+//                throw new WorkFlowException("当前只是简单的系统 没有RBAC功能,各位可以自己实现!");
             }
             else if(AssigneeTypeEnums.ROLE.getTypeName().equals(assignedType)){
-                throw new WorkFlowException("当前只是简单的系统 没有RBAC功能,各位可以自己实现!");
+
+//                throw new WorkFlowException("当前只是简单的系统 没有RBAC功能,各位可以自己实现!");
             }
             else if(AssigneeTypeEnums.SELF.getTypeName().equals(assignedType)){
                 String startUserJson = execution.getVariable(START_USER_INFO, String.class);
