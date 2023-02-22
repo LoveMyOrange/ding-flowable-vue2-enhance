e4ece855cb284d3ce0ba59161dc142b22156a65f
dongjiahao <dongjiahao0427@gmail.com>
2023-02-22 17:41:02 +0800

    update:修改条件节点，新增其他条件Type

--- Front/src/views/workspace/process/AgreenForm.vue
+++ Front/src/views/workspace/process/AgreenForm.vue
@@ -6,23 +6,16 @@
       </el-form-item>
 
       <el-form-item label="上传附件:">
-        <el-upload ref="upfile"
-                   style="display: inline"
-                   :auto-upload="false"
-                   :on-change="handleChange"
-                   :file-list="fileList"
-                   action="#">
-          <el-button type="primary">选择文件</el-button>
-        </el-upload>
+   <FileUpload  :value="fileList"  ></FileUpload>
       </el-form-item>
 
-      <el-form-item label="签名画板" v-if="formData.signFlag">
-        <vue-esign ref="esign" :width="800" :height="300" :isCrop="isCrop" :lineWidth="lineWidth" :lineColor="lineColor"
-                   :bgColor.sync="bgColor"/>
+      <el-form-item label="签名画板" v-if="processInfo.signFlag">
+        <vue-esign ref="esign2" :width="800" :height="300" 
+                 />
 
-        <button @click="handleReset">清空画板</button>
+        <el-button type="warning" @click="handleReset">清空画板</el-button>
 
-        <button @click="handleGenerate">生成图片</button>
+        <el-button  type="primary"  @click="handleGenerate"  >生成图片</el-button>
       </el-form-item>
     </el-form>
   </WDialog>
@@ -30,8 +23,12 @@
 
 <script>
 import {agree} from '@/api/design'
-
+import FileUpload from '@/views/workspace/process/FileUpload.vue'
 export default {
+  components: { FileUpload},
+  props: {
+    fileList:[],
+  },
   data() {
     return {
       formData: {
@@ -40,36 +37,23 @@ export default {
       },
       processInfo: {},
       showDialog: false,
-      file: null,
-
+      signInfo: "",
     }
   },
   methods: {
-    handleReset() {
-      this.$refs['esign'].reset() //清空画布
+    httpRequest(){
+
     },
-    upload() {
-      let fd = new FormData();
-      fd.append("name", this.name);
-      this.fileList.forEach(item => {
-        //文件信息中raw才是真的文件
-        fd.append("files", item.raw);
-        //console.log(item.raw)
-      })
-      this.$axios.post('/uploadUi', fd).then(res => {
-        if (res.data.code === 200) {
-          //console.log(res);
-          this.file = res;
-          this.$message('上传成功')
-        } else {
-          this.$message('失败')
-        }
-      })
+    handleReset() {
+      this.$refs['esign2'].reset() //清空画布
     },
     handleGenerate() {
-      this.$refs['esign'].generate().then(res => {
+      this.$refs['esign2'].generate().then(res => {
        // this.resultImg = res // 得到了签字生成的base64图片
-        this.$message("生成签名成功")
+        this.signInfo = res;
+       console.log(res);
+       this.showDialog = true;
+       this.$message("生成成功")
       }).catch(err => { //  没有签名，点击生成图片时调用
         this.$message("未签名！")
       })
@@ -77,10 +61,13 @@ export default {
     initFrom(processInfo, callback) {
       this.callback = callback
       this.processInfo = processInfo
+      console.log("processInfo", processInfo)
       this.showDialog = true
     },
     onOk() {
-      const data = {...this.fromData, ...this.processInfo,}
+      let data = {...this.fromData, ...this.processInfo}
+      data.signInfo = this.signInfo;
+      console.log("data", data)
       agree(data).then(res => {
         console.log("onOk", res)
         this.showDialog = false
